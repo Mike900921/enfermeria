b51f55b62f2f1fd0565f1447392351fc93496b54
luis <mendezluisdavid23@gmail.com>
2026-03-12 10:00:26 -0500

    estadisticas

--- resources/views/estadisticas/index.blade.php
+++ resources/views/estadisticas/index.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.base')
 
 @section('content')
-    <div class="container mt-4">
-
-        <div class="row">
-
+<div class="container mt-4">
+    <div class="row">
   {{-- verificacion de errores de fechas --}}
 
     @if ($errors->any())
@@ -25,6 +23,7 @@
             @endforeach
         </div>
     @endif
+    </div>
 
     <div class="row">
 
@@ -68,44 +67,26 @@
 
 
         <div class="col-5">
-                <!--filtros para buscar-->
-                <div class="row">
-                    <form action="{{ route('estadisticas.index') }}" method="GET" class="card p-3 mb-4 shadow-sm">
-                        <div class="row align-items-end">
-                            <div class="col-md-3">
-                                <label class="form-label fw-bold">Desde:</label>
-                                <input type="date" name="fecha_inicio" class="form-control"
-                                    value="{{ request('fecha_inicio') }}">
-                            </div>
-                            <div class="col-md-3">
-                                <label class="form-label fw-bold">Hasta:</label>
-                                <input type="date" name="fecha_fin" class="form-control"
-                                    value="{{ request('fecha_fin') }}">
-                            </div>
-
-                            <div class="col-md-4">
-                                <label class="form-label fw-bold">Agrupar por:</label>
-                                <select name="ver" class="form-select">
-                                    <option value="ficha" {{ request('ver') == 'ficha' ? 'selected' : '' }}>Número de Ficha
-                                    </option>
-                                    <option value="programa" {{ request('ver') == 'programa' ? 'selected' : '' }}>Nombre
-                                        del Programa</option>
-                                </select>
-                            </div>
-
-                            <div class="col-md-2">
-                                <button type="submit" class="btn btn-primary w-100">
-                                    <i class="bi bi-search"></i>
-                                </button>
-                            </div>
+             <!--filtros para buscar-->
+            <div class="row">
+                <form action="{{ route('estadisticas.index') }}" method="GET" class="card p-3 mb-4 shadow-sm">
+                    <div class="row align-items-end">
+                        <div class="col-md-3">
+                            <label class="form-label fw-bold">Desde:</label>
+                            <input type="date" name="fecha_inicio" class="form-control"
+                                value="{{ request('fecha_inicio') }}">
                         </div>
-                    </form>
-                </div>
+                        <div class="col-md-3">
+                            <label class="form-label fw-bold">Hasta:</label>
+                            <input type="date" name="fecha_fin" class="form-control"
+                                value="{{ request('fecha_fin') }}">
+                        </div>
                         <div class="col-md-5">
                             <label class="form-label fw-bold">Agrupar por:</label>
                             <select name="ver" class="form-control">
                                 <option value="ficha" {{ request('ver') == 'ficha' ? 'selected' : '' }}>Número de Ficha</option>
                                 <option value="programa" {{ request('ver') == 'programa' ? 'selected' : '' }}>Nombre del Programa</option>
+
                             </select>
                         </div>
 
@@ -129,23 +110,19 @@
                             </button>
                         </div>
                     </div>
+                </form>
+            </div>
+            <div class="row">
+                <div class="col-12 card p-3 shadow-sm">
+                    <canvas id="myChart"></canvas>
                 </div>
-
-
-
-
             </div>
         </div>
-
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
-<script src="{{ asset('js/topbar.js') }}"></script>
 
 <script>
   // Registrar el plugin globalmente
