05f486fb0205642c9eeebe9ce9dd000c64bb97b1
Mike <mistorma@gmail.com>
2026-03-19 10:28:44 -0500

    pequeños cambis

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -11,7 +12,8 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/topbar.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <title>Document</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logoSena.png') }}">
+    <title>Enfermería</title>
 </head>
 <style>
     .nav-link.text-dark:hover {
